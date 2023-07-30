27be571efc33df41934fc043946fa1015fb1b3ef
Gabriel Cor <gabriel.cor@beacon42.com>
2023-07-29 20:05:37 -0400

    Emulating GET and POST as UPC

--- ArduinoCode/NanoEnc2860J/UPCNanoEmulator/src/main.cpp
+++ ArduinoCode/NanoEnc2860J/UPCNanoEmulator/src/main.cpp
@@ -1,16 +1,21 @@
 
 #include <SPI.h>
 #include <EthernetENC.h>
-#define DHCPSETUP
+ #define DHCPSETUP
 
 // Enter a MAC address and IP address for your controller below.
 // The IP address will be dependent on your local network:
 byte mac[] = {
-  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
+  0xDA, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
 };
 IPAddress ip(192, 168, 1, 177);
 char* EstadoPuzzle="UNSOLVED";
-String readString;
+char stringReceived[100];
+char postReceived[100];
+int lugReceived = 0;
+int lugpostReceived = 0;
+bool ComandoState = false;
+bool ComandoPost = false;
 
 // Initialize the Ethernet server library
 // with the IP address and port you want to use
@@ -31,20 +36,20 @@ void setup() {
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
-  Serial.println("Ethernet WebServer Example");
+  Serial.println(F("Ethernet WebServer Example"));
 
   // start the Ethernet connection and the server:
 #ifdef DHCPSETUP
 // start the Ethernet connection:
-  Serial.println("Initialize Ethernet with DHCP:");
+  Serial.println(F("Initialize Ethernet with DHCP:"));
   if (Ethernet.begin(mac) == 0) 
   {
-     Serial.println("ERROR can't configure via DHCP");
+     Serial.println(F("ERROR can't configure via DHCP"));
   }
   else
   {
     // print your local IP address:
-    Serial.print("My IP address: ");
+    Serial.print(F("My IP address: "));
     Serial.println(Ethernet.localIP());
   }
 
@@ -53,91 +58,135 @@ void setup() {
 #endif
   // Check for Ethernet hardware present
   if (Ethernet.hardwareStatus() == EthernetNoHardware) {
-    Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+    Serial.println(F("Ethernet shield was not found.  Sorry, can't run without hardware. :("));
     while (true) {
       delay(1); // do nothing, no point running without Ethernet hardware
     }
   }
   if (Ethernet.linkStatus() == LinkOFF) {
-    Serial.println("Ethernet cable is not connected.");
+    Serial.println(F("Ethernet cable is not connected."));
   }
 
   // start the server
   server.begin();
-  Serial.print("server is at ");
+  Serial.print(F("server is at "));
   Serial.println(Ethernet.localIP());
 }
 
-void SendMessage(  EthernetClient client)
-{
-           client.println("HTTP/1.1 200 OK");
-          client.println("Content-Type: text/html");
-          client.println("Connection: close");  // the connection will be closed after completion of the response
-          client.println("Refresh: 5");  // refresh the page automatically every 5 sec
-          client.println();
-          client.println("<!DOCTYPE HTML>");
-          client.println("<html>");
-          // output the value of each analog input pin
-          for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
-            int sensorReading = analogRead(analogChannel);
-            client.print("analog input ");
-            client.print(analogChannel);
-            client.print(" is ");
-            client.print(sensorReading);
-            client.println("<br />");
-          }
-          client.println("</html>");
-        
-}
 
 void SendMessage2(EthernetClient client)
 {
           char macAddress[18];
           sprintf(macAddress, "%02X:%02X:%02X:%02X:%02X:%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
-          
-          client.println("HTTP/1.1 200 OK");
-          client.println("Content-Type: text/html");
-          client.println("Connection: close");  // the connection will be closed after completion of the response
-          client.println("Refresh: 15");  // refresh the page automatically every 5 sec
-          client.println();
-          client.println("<!DOCTYPE HTML>");
-
-                    client.println("<html>");
-                    client.println("</html>");
           char elapsedSecondsStr[12];
           sprintf(elapsedSecondsStr, "%lu", millis()/1000);
-          client.print("<html><head><title>Emulando UPC</title></head><body><div style='text-align:center;'><h1>Arduino UPC emulator mac address - ");
-          client.print(macAddress);
-          client.print(" </h1>Tiempo transcurrido : ");
-          client.print(elapsedSecondsStr);
-          client.print(" segundos<br /><br />Estado del Puzzle: ");
-          client.print(EstadoPuzzle);
-          client.print(" <br /><a href=\"/?status=SOLVED\"><input type=\"button\" value=\"SOLVED\"></a><a href=\"/?status=UNSOLVED\"><input type=\"button\" value=\"UNSOLVED\"></a><br /><br /></body></html>");
 
+          if (!ComandoState)
+          {
+            client.println(F("HTTP/1.1 200 OK"));
+            client.println(F("Content-Type: text/html"));
+            client.println(F("Connection: close"));  // the connection will be closed after completion of the response
+            client.println(F("Refresh: 15"));  // refresh the page automatically every 5 sec
+            client.println();
+            client.println(F("<!DOCTYPE HTML>"));
+
+            client.println(F("<html>"));
+            client.println(F("</html>"));
+        
+            client.print(F("<html><head><title>Emulando UPC</title></head><body><div style='text-align:center;'><h1>Arduino UPC emulator mac address - "));
+            client.print(macAddress);
+            client.print(F(" </h1>Tiempo transcurrido : "));
+            client.print(elapsedSecondsStr);
+            client.print(F(" segundos<br /><br />Estado del Puzzle: "));
+            client.print(EstadoPuzzle);
+            client.print(F(" <br /><a href=\"/?status=SOLVED\"><input type=\"button\" value=\"SOLVED\"></a><a href=\"/?status=UNSOLVED\"><input type=\"button\" value=\"UNSOLVED\"></a><br /><br /></body></html>"));
+          }
+          else
+          {
+            char* machineState = "0";
+            char* outputState = "0";
+            if (EstadoPuzzle == "SOLVED")
+            {
+              machineState = "2";
+              outputState = "1";
+            }
+
+            client.print(F("HTTP/1.0 200 OK\r\n"));
+            client.println(F("Content-Type: text/json\r\n"));
+//             client.print(F("{ \"id\":\"3\"} \r\n"));
+             client.print(F("{ \"id\":\""));
+             client.print(macAddress);
+             client.print(F("\", \"sv\":\"01.01.06\", \"hv\":\"5\", \"puzzle_state\":{ \"number_of_steps\":\"10\", \"current_step\":\"9\", \"attempt_number\":\"0\"}, \"universal_state\":{ \"machine_state\":\""));
+             client.print(machineState);
+             client.print(F("\", \"output_state\":\""));
+             client.print(outputState);
+             client.println(F("\", \"input_states\":\"65535\", \"overwritten_inputs\":\"0\"}, \"message\":{ \"type\":\"info\", \"content\":\"OK!\"} }"));
+          }
 }
 
 void loop()
 {
   EthernetClient client = server.available();
   if (client) {
-    bool currentLineIsBlank = true;
+    bool currentLineIsBlank = false;
+    ComandoState= false;
+    ComandoPost = false;
     while (client.connected()) {
       if (client.available()) {
         char c = client.read();
-        readString += c;
-        // Serial.write(c);
-        if (c == '\n' && currentLineIsBlank) {
-          // send a standard HTTP response header
-          Serial.println(readString);
-          Serial.println(readString.length());
+        if (!(lugReceived > 99)) {
+           stringReceived[lugReceived] = c;
+           lugReceived++;
+        }
 
-          // blank readString
-          readString="";
+        if (c == '\n' && currentLineIsBlank) {
+          if (ComandoPost)
+          {
+            lugpostReceived = 0;
+            while(client.available())
+            {
+              postReceived[lugpostReceived] = client.read();
+              lugpostReceived++;
+            }
+            Serial.println(postReceived);
+            
+             if(strstr((char *)postReceived, "\"universal_set\":{\"machine_state\":") != 0) {
+                Serial.println(F("Comando POST de cambio reconocido"));
+                if (strstr((char *)postReceived, ":2}") != 0)
+                {
+                  Serial.println(F("Comando SOLVED recibido"));
+                  EstadoPuzzle = "SOLVED";
+                }
+                if (strstr((char *)postReceived, ":0}") != 0)
+                {
+                  Serial.println(F("Comando UNSOLVED recibido"));
+                  EstadoPuzzle = "UNSOLVED";
+                }
+            }
+          }
           SendMessage2(client);
-            break;
+          break;
         }
         if (c == '\n') {
           // you're starting a new line
+          Serial.println(stringReceived);
+          if(strstr((char *)stringReceived, "GET /?command=state") != 0) {
+            Serial.println(F("Comando state recibido"));
+            ComandoState = true;
+          }
+          if(strstr((char *)stringReceived, "GET /?status=SOLVED") != 0) {
+            Serial.println(F("Comando SOLVED recibido"));
+            EstadoPuzzle = "SOLVED";
+          }
+          if(strstr((char *)stringReceived, "GET /?status=UNSOLVED") != 0) {
+            Serial.println(F("Comando UNSOLVED recibido"));
+            EstadoPuzzle = "UNSOLVED";
+          }
+          if(strstr((char *)stringReceived, "POST / HTTP/1.1") != 0) {
+            Serial.println(F("Comando POST recibido"));
+            ComandoPost = true;
+          }
+          lugReceived=0;
           currentLineIsBlank = true;
         } else if (c != '\r') {
           // you've gotten a character on the current line
@@ -153,41 +202,4 @@ void loop()
   }
 
 }
-void Okloop() {
-  // listen for incoming clients
-  EthernetClient client = server.available();
-  if (client) {
-    Serial.println("new client");
-    // an HTTP request ends with a blank line
-    bool currentLineIsBlank = true;
-    while (client.connected()) {
-      if (client.available()) {
-        char c = client.read();
 
-        Serial.write(c);
-        // if you've gotten to the end of the line (received a newline
-        // character) and the line is blank, the HTTP request has ended,
-        // so you can send a reply
-        if (c == '\n' && currentLineIsBlank) {
-          // send a standard HTTP response header
-          SendMessage2(client);
-            break;
-        }
-        if (c == '\n') {
-          // you're starting a new line
-          currentLineIsBlank = true;
-
-
-        } else if (c != '\r') {
-          // you've gotten a character on the current line
-          currentLineIsBlank = false;
-        }
-      }
-    }
-    // give the web browser time to receive the data
-    delay(1);
-    // close the connection:
-    client.stop();
-    Serial.println("client disconnected");
-  }
-}
