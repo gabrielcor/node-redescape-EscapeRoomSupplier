ca2b084662b88e8611357fdeb4dd4eadb2fc218a
Gabriel Cor <gabriel.cor@beacon42.com>
2023-07-30 20:25:56 -0400

    Initial wifi connectivity working

---
+++ ESP32Code/UPCEsp32Emulator/include/wifi_config.h
@@ -0,0 +1,3 @@
+// WiFi configuration
+const char *ssid = "Hamburgo 101 5G Nova";
+const char *password = "8001017170";
--- ESP32Code/UPCEsp32Emulator/src/main.cpp
+++ ESP32Code/UPCEsp32Emulator/src/main.cpp
@@ -1,4 +1,15 @@
-#include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <ArduinoJson.h>
+#include <wifi_config.h>
+
+WiFiClient networkClient;
+char *deviceMacAddress; // MAC address of the device
+char deviceNamestr[] = "";
+char *deviceName = deviceNamestr;       // Name of the device
+char deviceIDstr[] = "UNCONFIGURED";
+char *deviceID = deviceIDstr;
+
 
 // put function declarations here:
 int myFunction(int, int);
@@ -6,6 +17,28 @@ int myFunction(int, int);
 void setup() {
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
+  Serial.begin(115200);
+  
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  // Print the IP address
+  Serial.print(F("IP Address: "));
+  Serial.println(WiFi.localIP());
+
+  // Get Mac Address and assign it to deviceMacAddress
+  Serial.print(F("MAC Address: "));
+  String macAddress = WiFi.macAddress();
+  macAddress.toCharArray(deviceMacAddress, 32);
+  Serial.println(macAddress);
+  // REMOVE ":" from macAddress
+  macAddress.replace(":", "");
+  
 }
 
 void loop() {
